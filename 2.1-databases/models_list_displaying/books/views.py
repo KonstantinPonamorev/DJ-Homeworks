09b91e9305ec41198b4b1181b62d9edb78b7da64
Konstantin Ponamorev <kostyaponamorev@gmail.com>
2022-05-08 13:24:05 +0300

    Models_list_displaying

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,27 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+from books.models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books_objects = Book.objects.order_by('pub_date')
+    context = {
+        'books': books_objects
+    }
     return render(request, template, context)
+
+
+
+def books_pub_date(request, pub_date):
+    template = 'books/books_list.html'
+    books_objects = Book.objects.filter(pub_date=pub_date)
+    # books_objects = Book.objects.order_by('pub_date')
+    # paginator = Paginator(books_objects, 5)
+    # page = paginator.get_page(1)
+    context = {
+        'books': books_objects
+    }
+    return render(request, template, context)
+

3458d7d937a537d983bf565bc89612614f24a8a3
Konstantin Ponamorev <kostyaponamorev@gmail.com>
2022-06-13 17:29:34 +0300

    3.3-permissions main done

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -1,12 +1,17 @@
-from django_filters import rest_framework as filters
+from django_filters import rest_framework as filters, DateFromToRangeFilter, ChoiceFilter, CharFilter, ModelChoiceFilter
+from rest_framework.authtoken.admin import User
 
-from advertisements.models import Advertisement
+from advertisements.models import Advertisement, AdvertisementStatusChoices
 
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
 
-    # TODO: задайте требуемые фильтры
+    created_at = DateFromToRangeFilter()
+    updated_at = DateFromToRangeFilter()
+    status = ChoiceFilter(choices=AdvertisementStatusChoices.choices)
+    creator = ModelChoiceFilter(queryset=User.objects.all())
 
     class Meta:
         model = Advertisement
+        fields = ['creator', 'status', 'created_at', 'updated_at']

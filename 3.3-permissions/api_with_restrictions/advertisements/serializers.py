3626710a89f902f2d2751d227246f9c976f50ad7
Konstantin Ponamorev <kostyaponamorev@gmail.com>
2022-06-14 17:00:09 +0300

    3.3-permissions main done 1.1

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -35,6 +35,9 @@ class AdvertisementSerializer(serializers.ModelSerializer):
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
 
-        if Advertisement.objects.filter(creator=self.context["request"].user).filter(status='OPEN').count() >= 10:
-            raise ValidationError('Максимум = 10 открытых объявлений. Tip: удалите старое')
+        if self.context['view'].action == 'create':
+            if 'status' not in data or data['status'] == ('OPEN' or 'Открыто'):
+                if Advertisement.objects.filter(creator=self.context["request"].user).filter(status='OPEN').count() >= 10:
+
+                    raise ValidationError('Максимум = 10 открытых объявлений. Tip: удалите старое')
         return data
